19afc283a275541c19b5f811922979815c12b442
Eraj Mirzoev <mirzoeva145@gmail.com>
2025-07-26 15:42:33 +0500

    created carCard

--- src/app/auto/page.tsx
+++ src/app/auto/page.tsx
@@ -1,7 +1,11 @@
+import CarCard from "@/components/carCard/carCard";
+
 export default function About() {
   return (
 	 <main className="about">
 		<h1 className="about-title">Automobile page</h1>
+		<CarCard/>
+		
 	 </main>
   );
 }
